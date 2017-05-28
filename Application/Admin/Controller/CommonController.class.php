1542e20c08d6580ae3bc5f6c36859e05fcbddab8
dangjin <dangjin798@yeah.net>
2017-05-28 17:03:46 +0800

    CategoryController
    Signed-off-by: dangjin <dangjin798@yeah.net>

---
+++ Application/Admin/Controller/CommonController.class.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DangJin
+ * Date: 2017/5/28
+ * Time: 17:02
+ */
+
+namespace Admin\Controller;
+
+
+use Think\Controller;
+
+class CommonController extends Controller
+{
+
+}
---
+++ Application/Admin/Controller/IndexController.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DangJin
+ * Date: 2017/5/28
+ * Time: 17:02
+ */
+
+namespace Admin\Controller;
+
+
+class IndexController extends CommonController
+{
+
+}
