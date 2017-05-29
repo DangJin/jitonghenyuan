19ffe88b8476c5ea173841a955c0d6c98dfb92ef
Liudel <18232450029@163.com>
2017-05-29 09:47:15 +0800

    aaa

--- Application/Home/Controller/AboutUsController.class.php
+++ Application/Home/Controller/AboutUsController.class.php
@@ -10,10 +10,16 @@ namespace Home\Controller;
 
 use Think\Controller;
 
+
 class AboutUsController extends Controller
 {
     public function index()
     {
         $this->display('aboutUs');
+    }
+
+    public function aa()
+    {
+
     }
 }
